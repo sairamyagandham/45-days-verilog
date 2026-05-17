f3ca4d2054957c152c93bf27c92a154230ac6a71
sairamyagandham <sairamyagandham@gmail.com>
2026-05-17 10:12:48 +0530

    Create tb_sipo_shift_register.v

---
+++ DAY21/tb_sipo_shift_register.v
@@ -0,0 +1,23 @@
+`timescale 1ns/1ps
+module tb_sipo_shift_register;
+  reg clk,data_in;
+  wire [3:0] q;
+  sipo_shift_register uut(
+    .clk(clk),
+    .data_in(data_in),
+    .q(q)
+  );
+  always #5 clk=~clk;
+  initial begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0,tb_sipo_shift_register);
+    clk=0;
+      data_in = 1; #10;
+    data_in = 0; #10;
+    data_in = 1; #10;
+    data_in = 1; #10;
+    data_in = 0; #10;
+
+    $finish;
+end
+endmodule
